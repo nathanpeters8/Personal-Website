a659d7fd15a1b030670cd05de969ebcecc87e649
Nate Peters <peters.nate8@gmail.com>
2023-02-23 21:29:18 -0700

    imported react router dom, created components for home, about, projects pages

---
+++ personal_website/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+const HomePage = () => (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        This is my home page! Proin congue ligula id risus posuere, vel eleifend ex
+        egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt egestas.
+        Nam consectetur varius turpis, non porta arcu porttitor non. In tincidunt
+        vulputate nulla quis egestas. Ut eleifend ut ipsum non fringilla. Praesent
+        imperdiet nulla nec est luctus, at sodales purus euismod.
+      </p>
+
+    </>
+  );
+  
+  export default HomePage;
+  
---
+++ personal_website/src/pages/ProjectsPage.js
@@ -0,0 +1,15 @@
+const ProjectsPage = () => (
+    <>
+      <h1>Projects</h1>
+      <p>
+        This is my projects page! Proin congue ligula id risus posuere, vel eleifend ex
+        egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt egestas.
+        Nam consectetur varius turpis, non porta arcu porttitor non. In tincidunt
+        vulputate nulla quis egestas. Ut eleifend ut ipsum non fringilla. Praesent
+        imperdiet nulla nec est luctus, at sodales purus euismod.
+      </p>
+    </>
+  );
+  
+  export default ProjectsPage;
+  
